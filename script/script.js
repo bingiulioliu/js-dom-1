ed229ceceba963a1460267c4659c39816b1d1d7a
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-02 16:03:08 +0200

    ADD switch lampadina

--- script/script.js
+++ script/script.js
@@ -6,7 +6,11 @@ FUNZIONI
 -------------------------------- */
 // Handler: istruzioni per switch immagine al click
 function clickOnOff() {
-    
+    if(lampadina.src.includes("img/white_lamp.png")) {
+        lampadina.src = "img/yellow_lamp.png";
+    }
+    else
+        lampadina.src = "img/white_lamp.png";
 
 }
 
