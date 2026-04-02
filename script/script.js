c5f561411b01db0a403c0d23a55491ad34174e98
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-02 16:09:13 +0200

    ADD alt e text

--- script/script.js
+++ script/script.js
@@ -8,11 +8,15 @@ FUNZIONI
 function clickOnOff() {
     if(lampadina.src.includes("img/white_lamp.png")) {
         lampadina.src = "img/yellow_lamp.png";
+        lampadina.alt="lampadina accesa";
+        bottone.textContent = 'Spegni';
     }
     else
         lampadina.src = "img/white_lamp.png";
+        lampadina.alt="lampadina spenta";
+        bottone.textContent = 'Accendi';
 
 }
 
-
+// Collegamento bottone-funzione
 bottone.addEventListener('click', clickOnOff);
