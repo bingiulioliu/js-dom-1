667c67dae5d7bd065f969130ecc288a6007111c4
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-02 15:58:20 +0200

    ADD id e collegamenti

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const bottone = document.querySelector('#btn-toggle');
+const lampadina = document.querySelector('#lampadina-on-off');
+
+/* --------------------------------
+FUNZIONI
+-------------------------------- */
+// Handler: istruzioni per switch immagine al click
+function clickOnOff() {
+    
+
+}
+
+
+bottone.addEventListener('click', clickOnOff);
